d16b3aa61796f27686a24dfc556c568bc2414493
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-18 12:06:58 +0530

    Finished onboarding

--- lib/data.dart
+++ lib/data.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 var pageList = [
   PageModel(
-      imageUrl: "assets/illustration.png",
+      imageUrl: "assets/1-1.png",
       title: "FOR YOU",
       body: "EXPERIENCE WICKED PLAYLISTS",
       titleGradient: [Color(0xFF9708CC), Color(0xFF43CBFF)]),
@@ -12,7 +12,7 @@ var pageList = [
       body: "FEEL THE MAGIC OF WELLNESS",
       titleGradient: [Color(0xFFE2859F), Color(0xFFFCCF31)]),
   PageModel(
-      imageUrl: "assets/illustration3.png",
+      imageUrl: "assets/3-3.png",
       title: "YOUR TIME",
       body: "LET'S HIKE UP!",
       titleGradient: [Color(0xFF5EFCE8), Color(0xFF736EFE)]),
